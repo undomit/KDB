26911a13a249aa2ac554fa2f11267d20029ae235
undomit <andumit@gmail.com>
2020-09-09 18:36:09 +0300

    Removed node_modules, updated README.md

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React,{Suspense} from 'react'
+import React, {Suspense} from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 
 ReactDom.render(
-  <App />,
+  <Suspense fallback = {null}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Suspense>,
   document.getElementById('root')
 )
